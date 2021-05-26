cc00cb4ebfb06e3d4d090a84138bc94e36e4d895
hyagobsantos <hyagobezerra64@gmail.com>
2021-05-26 13:35:55 -0300

    Criando metodos Depósito e saque

--- 01 - ByteBank/Program.cs
+++ 01 - ByteBank/Program.cs
@@ -10,6 +10,71 @@ namespace _01___ByteBank
     {
         static void Main(string[] args)
         {
+            //***********************************************AULA 01 PARTE 02******************************************************************
+            //ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            //contaDaGabriela.titular = "Gabriela";
+            //contaDaGabriela.agencia = 863;
+            //contaDaGabriela.numero = 863452;
+            //contaDaGabriela.saldo = 100;
+
+            //Console.WriteLine($"Titular : {contaDaGabriela.titular}");
+            //Console.WriteLine($"Agencia :{contaDaGabriela.agencia}");
+            //Console.WriteLine($"Numero :{contaDaGabriela.numero}");
+            //Console.WriteLine($"Saldo :{contaDaGabriela.saldo}");
+
+
+            //contaDaGabriela.saldo += 200;
+
+            //***********************************************AULA 02 PARTE 02******************************************************************
+
+            //ContaCorrente Conta = new ContaCorrente();
+
+            //Conta.titular = "Gabriela";
+
+            //Console.WriteLine(Conta.titular);
+            //Console.WriteLine(Conta.saldo);
+
+            //Console.ReadLine();
+
+            //***********************************************AULA 03 PARTE 02******************************************************************
+
+            //ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            //contaDaGabriela.titular = "Gabriela";
+            //contaDaGabriela.agencia = 863;
+            //contaDaGabriela.numero = 863452;
+
+
+            //ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+
+            //contaDaGabriela.titular = "Gabriela";
+            //contaDaGabriela.agencia = 863;
+            //contaDaGabriela.numero = 863452;
+
+            //Console.ReadLine();
+
+            //***********************************************AULA 04 PARTE 02******************************************************************
+
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+            bool resultadoSaque = contaDoBruno.Sacar(50);
+            Console.WriteLine(contaDoBruno.saldo);
+
+
+            contaDoBruno.Depositar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+
+
+
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
